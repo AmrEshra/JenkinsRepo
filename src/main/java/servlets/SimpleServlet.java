6cbcf9ac0e38443a6a94712e0eb18b71ec8f819f
Amr Eshra <amr.eshra@gmail.com>
2020-03-03 13:53:17 +0300

    .

--- src/main/java/servlets/SimpleServlet.java
+++ src/main/java/servlets/SimpleServlet.java
@@ -17,7 +17,7 @@ public class SimpleServlet extends HttpServlet {
       try {
       resp.getWriter().write("Hello World! Maven Web Project Example.");
       } catch (IOException e){
-    	  e.printStackTrace();
+
       }
    }
 }
