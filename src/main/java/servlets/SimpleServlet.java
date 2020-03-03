3b5d99bdad204d594be637c8d8f542232b601fe2
Amr Eshra <amr.eshra@gmail.com>
2020-03-03 15:34:45 +0300

    ز

--- src/main/java/servlets/SimpleServlet.java
+++ src/main/java/servlets/SimpleServlet.java
@@ -1,8 +1,5 @@
 package servlets;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,13 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class SimpleServlet extends HttpServlet {
    private static final long serialVersionUID = 1L;
    @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
       resp.setContentType("text/plain");
-      try {
-      resp.getWriter().write("Hello World! Maven Web Project Example.");
-      } catch (IOException e){
-    	  e.printStackTrace();
-    	  throw e;
-      }
    }
 }
